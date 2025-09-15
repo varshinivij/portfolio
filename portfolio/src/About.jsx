0344c5a0f56391b5619c51c5f00969813c90cb4c
Varshini Vijay <varshinivijay05@gmail.com>
2025-09-15 11:40:52 -0700

    files revamped

---
+++ portfolio/src/About.jsx
@@ -0,0 +1,9 @@
+
+export default function About(){
+    return <>
+    <div>   
+        <p> In pursuit of innovation — driving equitable solutions that reinvent, enhance, and rectify existing systems. Building a strong technical foundation with a deep understanding of the mathematical principles that underpin them. Small steps, repeated, create change.</p>
+    </div>
+    </>
+}
+
